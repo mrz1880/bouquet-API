f4d1bcb29a95b94927514f6479d60d908f9aaa50
Apollon-Mite <abdoula.voraev@gmail.com>
2021-03-08 16:34:32 +0100

    localhost to 8080

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 
 cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
 
