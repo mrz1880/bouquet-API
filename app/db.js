9fb5c1bc7dcebf668a3de462a09eab43f0bbd493
Apollon-Mite <abdoula.voraev@gmail.com>
2021-03-08 15:48:32 +0100

    test 2

--- app/db.js
+++ app/db.js
@@ -13,6 +13,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
         // createdAt: 'Michel'
         // updatedAt: 'Augustin'
     },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false;
+      }
+    }
 });
 
 module.exports = sequelize;
