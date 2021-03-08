5276e686f836fb86de69b2d95ea2dd2d8eaafa56
Apollon-Mite <abdoula.voraev@gmail.com>
2021-03-08 17:17:33 +0100

    cors now allows everyone

--- server.js
+++ server.js
@@ -17,19 +17,29 @@ app.use(express.static('public'));
 //app.use(cors());
 
 // cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
 
-  // response to preflight request
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  }
-  else {
-    next();
-  }
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  // response.header('Access-Control-Allow-Credentials', true);
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
+
+//   // response to preflight request
+//   if (req.method === 'OPTIONS') {
+//     res.sendStatus(200);
+//   }
+//   else {
+//     next();
+//   }
+// });
 
 app.use(upload.array());
 app.use((req, res, next) => {
