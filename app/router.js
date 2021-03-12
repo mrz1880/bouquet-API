0a4237f9c9fb80785ccf53eb6d9daac4a9a1ccf1
Anthony D <mrz1880@gmail.com>
2021-03-12 14:50:43 +0100

    now :id need to be an integer

--- app/router.js
+++ app/router.js
@@ -20,18 +20,18 @@ router.get('/', (req, res) => {
 
 /** Products */
 router.get('/products', productController.getAllProducts);
-router.get('/product/:id', productController.getOneProduct);
-router.get('/seller/:id/products', productController.getProductsFromSeller);
-router.post('/seller/:id/products', authorization, productController.addNewProduct)
+router.get('/product/:id(\\d+)', productController.getOneProduct);
+router.get('/seller/:id(\\d+)/products', productController.getProductsFromSeller);
+router.post('/seller/:id(\\d+)/products', authorization, productController.addNewProduct)
 // router.patch('/seller/:Sid/product/:Pid', productController.editOneProduct)
 
-router.patch('/image/:id', imageController.editOneImage)
+router.patch('/image/:id(\\d+)', imageController.editOneImage)
 
 
 /* Orders */
-router.get('/order/:id', orderController.getOneOrder);
-router.get('/seller/:id/orders', orderController.getSellerOrders);
-router.get('/customer/:id/orders', orderController.getCustomerOrders);
+router.get('/order/:id(\\d+)', orderController.getOneOrder);
+router.get('/seller/:id(\\d+)/orders', orderController.getSellerOrders);
+router.get('/customer/:id(\\d+)/orders', orderController.getCustomerOrders);
 
 
 
@@ -42,16 +42,16 @@ router.get('/categories', categoryController.getAllCategories);
 
 /* Customers */
 router.get('/customers', customerController.getAllCustomers);
-router.get('/customer/:id', customerController.getOneCustomer);
-router.patch('/customer/:id', authorization, customerController.editCustomerProfile);
+router.get('/customer/:id(\\d+)', customerController.getOneCustomer);
+router.patch('/customer/:id(\\d+)', authorization, customerController.editCustomerProfile);
 router.post('/customer/login', customerController.customerHandleLoginForm); // LOGIN
 router.post('/customer/signup', customerController.customerHandleSignupForm); // SIGNUP
 
 
 /* Sellers */
 router.get('/sellers', sellerController.getAllSellers);
-router.get('/seller/:id', sellerController.getOneSeller);
-router.patch('/seller/:id', authorization, sellerController.editSellerProfile);
+router.get('/seller/:id(\\d+)', sellerController.getOneSeller);
+router.patch('/seller/:id(\\d+)', authorization, sellerController.editSellerProfile);
 router.post('/seller/login', sellerController.sellerHandleLoginForm); // LOGIN
 router.post('/seller/signup', sellerController.sellerHandleSignupForm); // SIGNUP
 
