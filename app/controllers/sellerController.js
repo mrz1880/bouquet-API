599b3648da9f5115e0fac9fa509fd1b1d4868504
Apollon-Mite <abdoula.voraev@gmail.com>
2021-03-12 14:16:18 +0100

    display errors

--- app/controllers/sellerController.js
+++ app/controllers/sellerController.js
@@ -66,7 +66,7 @@ const sellerController = {
             //response.status(200).json(updatedSeller);
             
         } catch (error) {
-                    console.log(error);
+          res.status(500).json(error.toString())
         }
 
     },
